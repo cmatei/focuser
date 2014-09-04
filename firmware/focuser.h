1172f042c958a9c5c31aa5dd2d1522c8ee9b87b8
Matei Conovici <mconovici@gmail.com>
2014-09-04 20:59:49 +0300

    Version 2 of the focuser firmware

--- firmware/focuser.h
+++ firmware/focuser.h
@@ -1,21 +1,29 @@
 #ifndef __FOCUSER_H
 #define __FOCUSER_H
 
+#define TINYFOCUSER_VERSION 2
+
 enum {
-	CMD_GET_ENCODER = 1,	/* get current encoder value */
-	CMD_SET_ENCODER,	/* set current encoder value */
+	CMD_GET_ENCODER = 1,		     /* not used in V2 */
+	CMD_SET_ENCODER,		     /* not used in V2 */
+	CMD_GET_TARGET,                      /* not used in V2 */
+	CMD_SET_TARGET,                      /* not used in V2 */
 
-	CMD_GET_TARGET,		/* get target encoder value */
-	CMD_SET_TARGET,		/* set target encoder value */
+	CMD_SET_PWM,		             /* set hold/move PWM */
 
-	CMD_SET_PWM,		/* set movement/hold pwm */
+	CMD_GET_TEMPERATURE,	             /* get current temperature */
 
-	CMD_GET_TEMPERATURE,	/* get current temperature */
+	CMD_MOVE,			     /* not used in V2 */
+	CMD_STOP,			     /* not used in V2 */
 
-	CMD_MOVE,		/* start moving in specified direction */
-	CMD_STOP,		/* abort movement */
+	CMD_SET_SPEED,                       /* F_CPU/64 ticks per motor step */
 
-	CMD_SET_SPEED,          /* steps per tick */
+	CMD_GET_POSITIONS,	             /* get encoder/target values */
+	CMD_SET_POSITIONS,	             /* set encoder/target values */
+	CMD_EXECUTE,	                     /* stop/run */
+	CMD_SET_STEPPING,		     /* set stepping mode */
+
+	CMD_GET_VERSION = 255,	             /* get version to adjust command set */
 };
 
 #endif
--- firmware/main.c
+++ firmware/main.c
@@ -2,36 +2,34 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
 
 #include "ds1820.h"
 #include "usbdrv.h"
 
 #include "focuser.h"
 
-#define DEFAULT_PWM_MOVE 255
-#define DEFAULT_PWM_HOLD 128
-
-/* steps per second */
-#define STEP_HZ 500
-#define TICKS_PER_STEP ((F_CPU / 64) / STEP_HZ)
-
-uint32_t motor_position = (1UL << 31);
-uint32_t motor_target   = (1UL << 31);
-
-uint8_t move_power = DEFAULT_PWM_MOVE;
-uint8_t hold_power = DEFAULT_PWM_HOLD;
-
-uint8_t move_dir = 1;
-uint8_t movement = 0;
-
-uint8_t temperature[2];
-
 #define COIL_1A PB6
 #define COIL_1B PB1
 #define COIL_2A PB0
 #define COIL_2B PB7
 
-uint8_t motor_phases[8] = {
+
+PROGMEM const uint8_t phases_fullstep_wave[4] = {
+	(1 << COIL_1A) | (0 << COIL_1B) | (0 << COIL_2A) | (0 << COIL_2B),
+	(0 << COIL_1A) | (0 << COIL_1B) | (1 << COIL_2A) | (0 << COIL_2B),
+	(0 << COIL_1A) | (1 << COIL_1B) | (0 << COIL_2A) | (0 << COIL_2B),
+	(0 << COIL_1A) | (0 << COIL_1B) | (0 << COIL_2A) | (1 << COIL_2B),
+};
+
+PROGMEM const uint8_t phases_fullstep[4] = {
+	(1 << COIL_1A) | (0 << COIL_1B) | (1 << COIL_2A) | (0 << COIL_2B),
+	(0 << COIL_1A) | (1 << COIL_1B) | (1 << COIL_2A) | (0 << COIL_2B),
+	(0 << COIL_1A) | (1 << COIL_1B) | (0 << COIL_2A) | (1 << COIL_2B),
+	(1 << COIL_1A) | (0 << COIL_1B) | (0 << COIL_2A) | (1 << COIL_2B),
+};
+
+PROGMEM const uint8_t phases_halfstep[8] = {
 	(1 << COIL_1A) | (0 << COIL_1B) | (0 << COIL_2A) | (0 << COIL_2B),
 	(1 << COIL_1A) | (0 << COIL_1B) | (1 << COIL_2A) | (0 << COIL_2B),
 	(0 << COIL_1A) | (0 << COIL_1B) | (1 << COIL_2A) | (0 << COIL_2B),
@@ -42,51 +40,55 @@ uint8_t motor_phases[8] = {
 	(1 << COIL_1A) | (0 << COIL_1B) | (0 << COIL_2A) | (1 << COIL_2B),
 };
 
-uint8_t current_phase = 0;
+/* wave drive, or single phase on */
+#define DEFAULT_PWM_MOVE 255
+#define DEFAULT_PWM_HOLD 0
+
+static const uint8_t *motor_phases = phases_fullstep_wave;
+static uint8_t num_phases = 4;
+
+static uint8_t move_power = DEFAULT_PWM_MOVE;
+static uint8_t hold_power = DEFAULT_PWM_HOLD;
+
+
+/* steps per second */
+#define STEP_HZ 250
+#define TICKS_PER_STEP ((F_CPU / 64) / STEP_HZ)
+
+/* in miliseconds */
+#define POWER_CHANGE_DELAY 10
+
+static uint16_t positions[2] = { 32768, 32768 };
+#define motor_encoder (positions[0])
+#define motor_target  (positions[1])
+
+uint8_t temperature[2];
+
+static uint8_t version = TINYFOCUSER_VERSION;
+
+
+/* http://www.nongnu.org/avr-libc/user-manual/FAQ.html#faq_regbind */
+register uint8_t execute   asm("r2");
+register uint8_t moving    asm("r3");
+register uint8_t direction asm("r4");
+
+register uint8_t current_phase asm("r5");
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
 	usbRequest_t *rq = (usbRequest_t *) data;
 
 	switch (rq->bRequest) {
-	case CMD_GET_ENCODER:
-		usbMsgPtr = (void *) &motor_position;
-		return 4;
-
-	case CMD_SET_ENCODER:
-		//motor_position = ((uint32_t) rq->wValue.word << 16) + rq->wIndex.word;
-		((uint8_t *) &motor_position)[0] = rq->wIndex.bytes[0];
-		((uint8_t *) &motor_position)[1] = rq->wIndex.bytes[1];
-
-		((uint8_t *) &motor_position)[2] = rq->wValue.bytes[0];
-		((uint8_t *) &motor_position)[3] = rq->wValue.bytes[1];
-		break;
-
-	case CMD_GET_TARGET:
-		usbMsgPtr = (void *) &motor_target;
-		return 4;
-
-	case CMD_SET_TARGET:
-		//motor_target = ((uint32_t) rq->wValue.word << 16) + rq->wIndex.word;
-		((uint8_t *) &motor_target)[0] = rq->wIndex.bytes[0];
-		((uint8_t *) &motor_target)[1] = rq->wIndex.bytes[1];
-
-		((uint8_t *) &motor_target)[2] = rq->wValue.bytes[0];
-		((uint8_t *) &motor_target)[3] = rq->wValue.bytes[1];
-		break;
+	case CMD_GET_VERSION:
+		usbMsgPtr = (void *) &version;
+		return 1;
 
 	case CMD_GET_TEMPERATURE:
 		usbMsgPtr = (void *) &temperature;
 		return 2;
 
-	case CMD_STOP:
-		movement = 0;
-		break;
-
-	case CMD_MOVE:
-		move_dir = rq->wValue.bytes[0];
-		movement = 1;
-		OCR0A = move_power;
+	case CMD_SET_SPEED:
+		OCR1A = rq->wValue.word;
 		break;
 
 	case CMD_SET_PWM:
@@ -94,35 +96,62 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
 		hold_power = rq->wValue.bytes[1];
 		break;
 
-		/* debug only, F_CPU/64 ticks per step */
-	case CMD_SET_SPEED:
-		OCR1A = rq->wValue.word;
+	case CMD_SET_STEPPING:
+		switch (rq->wValue.bytes[0]) {
+		case 0:
+			motor_phases = phases_fullstep_wave;
+			num_phases = 4;
+			break;
+		case 1:
+			motor_phases = phases_fullstep;
+			num_phases = 4;
+			break;
+		case 2:
+			motor_phases = phases_halfstep;
+			num_phases = 8;
+			break;
+		}
+
+		execute = 0;
+		current_phase = 0;
+		break;
+
+	case CMD_GET_POSITIONS:
+		usbMsgPtr = (void *) positions;
+		return 4;
+
+	case CMD_SET_POSITIONS:
+		motor_encoder = rq->wIndex.word;
+		motor_target  = rq->wValue.word;
+
+		/* will need an explicit move command after positions set */
+		execute = 0;
 		break;
 	}
 	return 0;
 }
 
+
 /* NOTE: ISR_NOBLOCK is required for USB */
 ISR (TIMER1_COMPA_vect, ISR_NOBLOCK)
 {
-	if (!movement || (motor_position == motor_target)) {
-		movement = 0;
-		OCR0A = hold_power;
-		return;
-	}
 
-	if (move_dir) {
-		motor_position++;
+	if (!execute || !moving || (motor_encoder == motor_target))
+		return;
+
+	if (direction) {
+		motor_encoder++;
 		current_phase++;
 	} else {
-		motor_position--;
+		motor_encoder--;
 		current_phase--;
 	}
 
-	current_phase &= 7;
-	PORTB = motor_phases[current_phase];
-}
+	if (current_phase >= num_phases)
+		current_phase = 0;
 
+	PORTB = pgm_read_byte_near(motor_phases + current_phase);
+}
 
 int main()
 {
@@ -145,7 +174,7 @@ int main()
 	OCR0A  = DEFAULT_PWM_MOVE;
 
 	/* motor phasing in Timer1 overflow interrupt.
-	   Timer1 in CTC mode, clk_io/64, every 500 => 500Hz, maybe increase with tests
+	   Timer1 in CTC mode, clk_io/64, e.g. every 500 => 500Hz, maybe increase with tests
 	 */
 	OCR1A = TICKS_PER_STEP;
 	TCCR1A = (0 << COM1A1) | (0 << COM1A0) |
@@ -165,6 +194,22 @@ int main()
 	for (;;) {
 		usbPoll();
 		ds1820_state_machine();
+
+		if (!execute || (motor_encoder == motor_target)) {
+			moving = 0;
+
+			_delay_ms(POWER_CHANGE_DELAY);
+			OCR0A = hold_power;
+
+			continue;
+		}
+
+		direction = (motor_encoder < motor_target);
+
+		OCR0A = move_power;
+		_delay_ms(POWER_CHANGE_DELAY);
+
+		moving = 1;
 	}
 
 
